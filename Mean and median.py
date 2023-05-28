79fa685d7e760e57fcf008388cee001b602ad370
Ayesha Irshad <104616632+AyeshaIrshad1337@users.noreply.github.com>
2023-05-28 16:00:43 +0500

    Update Mean and median.py

--- Mean and median.py
+++ Mean and median.py
@@ -37,3 +37,45 @@ be_and_usa = food_consumption[(food_consumption['country']=='Belgium') | (food_c
 
 # Group by country, select consumption column, and compute mean and median
 print(be_and_usa.groupby('country')['consumption'].agg([np.mean,np.median]))
+# # Excerise 2
+# In the video, you learned that the mean is the sum of all the data points divided by the total number of data points, and the median is the middle value of the dataset where 50% of the data is less than the median, and 50% of the data is greater than the median. In this exercise, you'll compare these two measures of center.
+
+# pandas is loaded as pd, numpy is loaded as np, and food_consumption is available.
+
+#instructions 
+# Import matplotlib.pyplot with the alias plt.
+# Subset food_consumption to get the rows where food_category is 'rice'.
+# Create a histogram of co2_emission for rice and show the plot.
+
+# Import matplotlib.pyplot with alias plt
+import matplotlib.pyplot as plt
+
+# Subset for food_category equals rice
+rice_consumption = food_consumption[food_consumption['food_category']=='rice']
+
+# Histogram of co2_emission for rice and show plot
+plt.hist(rice_consumption['co2_emission'])
+plt.show()
+
+
+# Question
+# Take a look at the histogram you just created of different countries' CO2 emissions for rice. Which of the following terms best describes the shape of the data?
+
+#  Answer is right skew
+
+
+# Use .agg() to calculate the mean and median of co2_emission for rice.
+
+# Subset for food_category equals rice
+rice_consumption = food_consumption[food_consumption['food_category'] == 'rice']
+
+# Calculate mean and median of co2_emission with .agg()
+print(rice_consumption['co2_emission'].agg([np.mean,np.median]))
+
+
+# Question
+# Given the skew of this data, what measure of central tendency best summarizes the kilograms of CO2 emissions per person per year for rice?
+
+# Possible answers
+
+# MEdian as it is representing the answer near the majority of the numbers are graphed
